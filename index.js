39e95c46777fe2439191721b004235099d6debe5
ericatmsantos <erica.tiago@gmail.com>
2024-09-17 22:30:32 -0300

    deletar metas

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ const listarMetas = async () => {
     return
   }
 
-
   respostas.forEach((resposta) => {
     const meta = metas.find((m) => {
       return m.value == resposta
@@ -80,6 +79,29 @@ const metasAbertas = async () => {
   })
 }
 
+const deletarMetas = async () => {
+  const metasDesmarcadas = metas.map((meta) => { //o map vai modificar o array original
+      return {value: meta.value, checked: false}
+  })
+  const itensADeletar = await checkbox ({
+    message: "Selecione item para deletar.",
+    choices: [...metasDesmarcadas],
+    instructions: false,
+  })
+
+  if(itensADeletar.length ==0) {
+    console.log("Nenhum item para deletar!")
+    return
+  }
+
+  itensADeletar.forEach((item) => {
+    metas = metas.filter((meta) => {
+      return meta.value != item
+    })
+  })
+
+  console.log("Meta(s) deletada(s) com sucesso!")
+}
 
 const start = async () => {
 
@@ -105,6 +127,10 @@ const start = async () => {
           value: "abertas"
         },
         {
+          name: "Deletar metas",
+          value: "deletar"
+        },
+        {
           name: "Sair",
           value: "sair"
         }
@@ -124,6 +150,9 @@ const start = async () => {
         break
       case "abertas":
         await metasAbertas()
+        break
+      case "deletar":
+        await deletarMetas()
         break
       case "sair":
         console.log("Até a próxima")
