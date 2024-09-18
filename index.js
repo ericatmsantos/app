d4700003664451113a28feb934c600b644c6a635
ericatmsantos <erica.tiago@gmail.com>
2024-09-17 21:51:12 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const listarMetas = async () => {
   metas.forEach((m) => {
     m.checked = false
   })
-  
+
   if(respostas.length == 0) {
     console.log("Nenhuma meta selecionada!")
     return
@@ -53,16 +53,33 @@ const metasRealizadas = async () => {
   })
 
 if(realizadas.length == 0){
-  console.log("Não existem metas realizadas!")
+  console.log("Não existem metas realizadas! :(")
   return
 }
 
 await select({
-  message: "Metas Realizadas",
+  message: "Metas Realizadas: " + realizadas.length,
   choices: [...realizadas]
 })
 
 }
+
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true /*lógica inversa*/
+  })
+
+  if(abertas.length == 0) {
+    console.log("Não existem metas abertas! :)")
+    return
+  }
+
+  await select({
+    message: "Metas Abertas: " + abertas.length,
+    choices: [...abertas]
+  })
+}
+
 
 const start = async () => {
 
@@ -84,6 +101,10 @@ const start = async () => {
           value: "realizadas"
         },
         {
+          name: "Metas abertas",
+          value: "abertas"
+        },
+        {
           name: "Sair",
           value: "sair"
         }
@@ -100,6 +121,9 @@ const start = async () => {
         break
       case "realizadas":
         await metasRealizadas()
+        break
+      case "abertas":
+        await metasAbertas()
         break
       case "sair":
         console.log("Até a próxima")
